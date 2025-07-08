63143fbc6519a2a7a3fe58b75046b09420cc796d
blu03 <nahhhh03@gmail.com>
2025-07-08 22:47:59 +0900

    modify test webapp

--- EcommerceApp/src/main/java/com/conn/DBConnect.java
+++ EcommerceApp/src/main/java/com/conn/DBConnect.java
@@ -14,8 +14,8 @@ public class DBConnect
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
-			conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
-			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "qwer123$");
+
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
